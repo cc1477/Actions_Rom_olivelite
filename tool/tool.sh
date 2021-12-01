dd043f358d605877b76cf638cf9d3dcdceccb791
gifyic <2870926414@qq.com>
2021-12-01 14:24:06 +0800

    fix: 修复vendor大小没有调整成功

--- tool/tool.sh
+++ tool/tool.sh
@@ -70,7 +70,7 @@ $bin/mke2fs -L / -t ext4 -b 4096 $n/system.img ${size}M > /dev/null 2>&1
 $bin/e2fsdroid -e -T 0 -S $n/config/system_file_contexts -C $n/config/system_fs_config  -a /system -f $n/system $n/system.img > /dev/null && rm -rf $n/system
 echo "$(date "+[ %H:%M:%S ]")  合成vendor"
 ssize=$(cat ${n}/config/vendor_size.txt)
-size=$(expr $ssize / 1024 / 1024 )
+size=1024
 $bin/mke2fs -L / -t ext4 -b 4096 $n/vendor.img ${size}M > /dev/null 2>&1
 $bin/e2fsdroid -e -T 0 -S $n/config/vendor_file_contexts -C $n/config/vendor_fs_config  -a /vendor -f $n/vendor $n/vendor.img > /dev/null && rm -rf $n/vendor
 #img转dat
