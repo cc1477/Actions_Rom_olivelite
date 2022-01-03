6a793184b9b911370a19fcabc4ddb59e625b90e2
gifyic <2870926414@qq.com>
2022-01-03 16:08:29 +0800

    fix bug

--- tool/Sub/build/run.sh
+++ tool/Sub/build/run.sh
@@ -4,10 +4,10 @@ PROJECT=$n					#工程目录
 SHELL=$(readlink -f "$0")			#脚本文件
 SHELL_PATH=${sub}/${lu}	#脚本路径
 
-sed -i 's/Redmi 8/Redmi 8a/' /vendor/odm/etc/build.prop
-sed -i 's/Redmi 8/Redmi 8a/' /system/build.prop
-sed -i 's/Redmi 8/Redmi 8a/' /vendor/build.prop
+sed -i 's/Redmi 8/Redmi 8a/' $PROJECT/vendor/odm/etc/build.prop
+sed -i 's/Redmi 8/Redmi 8a/' $PROJECT/system/system/build.prop
+sed -i 's/Redmi 8/Redmi 8a/' $PROJECT/vendor/build.prop
 
-sed -i 's/olive/olivelite/g' /vendor/odm/etc/build.prop
-sed -i 's/olive/olivelite/g' /system/build.prop
-sed -i 's/olive/olivelite/g' /vendor/build.prop
+sed -i 's/olive/olivelite/g' $PROJECT/vendor/odm/etc/build.prop
+sed -i 's/olive/olivelite/g' $PROJECT/system/system/build.prop
+sed -i 's/olive/olivelite/g' $PROJECT/vendor/build.prop
